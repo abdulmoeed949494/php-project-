4f2194c24c508515ba6aee67fbdb68ceec8ad1ce
huzaifarabnawaz <hrabnaw@gmail.com>
2024-05-07 06:58:22 -0700

    add footer in style .css

--- partial/footer.php
+++ partial/footer.php
@@ -1,10 +1,18 @@
+<style>
 
+.st{
+   align-items: center;
+   margin-top: 100px  ;
+   text-align: center;
+   
+   
+}
 
-
+</style>
  
- <div class="container-fluid bg-dark text-light my-4">
+ <div class="container-fluid bg-dark text-light my-4 ">
 
-    <p class="text-center"> COPY RIGHT HATECH</p>
+    <p class="text-center"><footer class="st"> COPY RIGHT HATECH</footer></p>
 
     </div>
 
